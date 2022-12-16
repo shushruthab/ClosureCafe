ac6a0cbcf2960d0996ff3f9656dbca9ae1274046
shushruthab <shushruthabhatt@gmail.com>
2022-12-15 22:55:27 -0800

    complete testing of unroll algorithm

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,64 @@
-function unroll(squareArray) {
 
+
+const rightTraverse = (squareArray, resultArray) => {
+    for (let i=0; i<squareArray[0].length; i++) {
+        resultArray.push(squareArray[0][i]);
+    }
+    squareArray.shift();
+    return squareArray;
+}
+
+const traverseDown = (rectangleArray, resultArray) => {
+    let verticalIdx = rectangleArray.length - 1;
+    let horizontalidx = rectangleArray[0].length - 1;
+    for (let i=0; i<=verticalIdx; i++) {
+        resultArray.push(rectangleArray[i][horizontalidx]);
+        rectangleArray[i].pop();
+    }
+    return rectangleArray;
+}
+
+const leftTraverse = (squareArray, resultArray) => {
+    const idx = squareArray.length - 1; 
+    for (let i = idx; i > -1; i--) {
+        resultArray.push(squareArray[idx][i]);
+    }
+    squareArray.pop();
+    return squareArray;
+}
+
+const traverseUp = (rectangleArray, resultArray) => {
+    let vIdx = rectangleArray.length - 1;
+
+    for (let i=vIdx; i> -1; i--) {
+        resultArray.push(rectangleArray[i][0]);
+        rectangleArray[i].shift();
+    }
+    return rectangleArray;
+}
+
+function unroll(squareArray, resultArray=[]) {
+    if (squareArray.length === 0) return resultArray;
+    if (squareArray.length === 1) {
+        resultArray.push(squareArray[0][0]);
+        return resultArray;
+        }
+    
+
+    squareArray = rightTraverse(squareArray, resultArray);
+    squareArray = traverseDown(squareArray, resultArray);
+    
+    if (squareArray.length === 0) return resultArray;
+    if (squareArray.length === 1) {
+        resultArray.push(squareArray[0][0]);
+        return resultArray;
+    }
+
+    squareArray = leftTraverse(squareArray, resultArray);
+    squareArray = traverseUp(squareArray, resultArray);
+
+    unroll(squareArray, resultArray);
+    return resultArray;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -1,9 +1,56 @@
 const unroll = require("./unroll");
 
+const onen = [["a"]];
+const twon = [
+  [1, 2],
+  [3, 4]
+]
+
+const threen = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+const fourn = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16]
+];
+
+const fiven = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25]
+]
+const fivenres = [1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13]
+
 describe("#unroll", function () {
 
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+});
+
+describe("#unroll", function () {
+
+  it("returns the correct values for small arrays", function () {
+    expect(unroll([[]])).toEqual([]);
+    expect(unroll([])).toEqual([]);
+    expect(unroll(onen)).toEqual(["a"]);
+    expect(unroll(twon)).toEqual([1, 2, 4, 3]);
+  })
+});
+
+describe("#unroll", function () {
+
+  it("returns the correct values for large arrays", function () {
+    expect(unroll(threen)).toEqual([1, 2, 3, 6, 9 ,8 ,7, 4, 5]);
+    expect(unroll(fourn)).toEqual([1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]);
+    expect(unroll(fiven)).toEqual(fivenres);
+  })
 });
